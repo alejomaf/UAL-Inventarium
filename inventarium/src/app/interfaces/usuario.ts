b29d79937b0c714add936ccaba913556e0493856
Alejo Martín Arias Filippo <alejomaf@outlook.com>
2021-07-06 18:52:21 +0200

    Login and register view created

---
+++ inventarium/src/app/interfaces/usuario.ts
@@ -0,0 +1,10 @@
+export interface Usuario {
+    idUsuario?: number,
+    nombre: Text,
+    correoElectronico: Text,
+    contrasena: Text,
+    rango: number,
+    departamento: number,
+    telefono: number,
+    user_token: String
+}
