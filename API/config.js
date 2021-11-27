6ffb4466cfb2026eb2f5ec3b966196ebdcbe9bac
Alejo Martín Arias Filippo <alejomaf@outlook.com>
2021-11-27 01:23:55 +0100

    Updating packages 2

--- API/config.js
+++ API/config.js
@@ -3,8 +3,8 @@ const env = process.env;
 const config = {
   db: {
     host: env.DB_HOST || 'localhost',
-    user: env.DB_USER || 'root',
-    password: env.DB_PASSWORD || '',
+    user: env.DB_USER || 'user',
+    password: env.DB_PASSWORD || 'user',
     database: env.DB_NAME || 'ualinventarium'
   },
   listPerPage: env.LIST_PER_PAGE || 10,
