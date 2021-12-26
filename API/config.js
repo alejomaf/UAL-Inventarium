baba8b90fa01ddfac1200c506509a4ac8759c035
alejomaf <alejomaf@outlook.com>
2021-12-26 18:02:59 +0000

    Empezamos con la redaccion del proyecto

--- API/config.js
+++ API/config.js
@@ -4,7 +4,7 @@ const config = {
   db: {
     host: env.DB_HOST || 'localhost',
     user: env.DB_USER || 'user',
-    password: env.DB_PASSWORD || 'user',
+    password: env.DB_PASSWORD || '21$uS3rP4ssM0Ord20D1f1CIL3',
     database: env.DB_NAME || 'ualinventarium'
   },
   listPerPage: env.LIST_PER_PAGE || 10,
