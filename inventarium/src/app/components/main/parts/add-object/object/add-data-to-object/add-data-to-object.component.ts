f57fc2be96d299c8fcf673b1b556ec95f9269905
Alejo Martín Arias Filippo <alejomaf@outlook.com>
2022-01-01 19:49:27 +0100

    Creación de objetos finalizada

--- inventarium/src/app/components/main/parts/add-object/object/add-data-to-object/add-data-to-object.component.ts
+++ inventarium/src/app/components/main/parts/add-object/object/add-data-to-object/add-data-to-object.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GroupOfObjectsService } from 'src/app/services/group-of-objects.service';
+import { ObjectsService } from 'src/app/services/objects.service';
 
 @Component({
   selector: 'app-add-data-to-object',
@@ -7,9 +11,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDataToObjectComponent implements OnInit {
 
-  constructor() { }
+  codigo = new FormControl("");
+  date = new Date()
+  fechaAdquisicion = new FormControl(this.date.getUTCFullYear() + "-" + this.date.getUTCMonth() + "-" + this.date.getUTCDate());
+  etiqueta = new FormControl("");
+  departamento = new FormControl(0);
+  mejoras = new FormControl("");
+  observaciones = new FormControl("");
+
+  idGrupoObjetos: any;
+  idLocalizacion: any;
+  cantidad: number;
+  tipo: any;
+
+  constructor(private router: Router, private route: ActivatedRoute, private objectS: ObjectsService) {
+    this.idGrupoObjetos = route.snapshot.params['group_of_object_id'];
+    this.idLocalizacion = route.snapshot.params['location_id'];
+    this.cantidad = route.snapshot.params['quantity'];
+  }
 
   ngOnInit(): void {
   }
 
+  generar() {
+    if (this.cantidad == 1) {
+      //Is the last item that we are going to generate
+      this.crearObjeto();
+      this.router.navigateByUrl('group-of-object/' + this.idGrupoObjetos);
+    }
+    else {
+      this.crearObjeto();
+      //We reduce the quantity in one unit for the next iteration
+      this.cantidad -= 1;
+      this.router.navigateByUrl('add-object/create/' + this.idGrupoObjetos + '/' + this.idLocalizacion + '/' + this.cantidad);
+    }
+  }
+
+  saltarCreaciones() {
+    const formData = new FormData();
+
+    formData.append("Ubicacion_idUbicacion", this.idLocalizacion);
+
+    for (let i = 0; i < this.cantidad; i++) {
+      this.objectS.addObject(formData, this.idGrupoObjetos).subscribe(
+        (res: any) => {
+          return;
+        });
+    }
+
+    this.router.navigateByUrl('group-of-object/' + this.idGrupoObjetos);
+  }
+
+  crearObjeto() {
+    const formData = new FormData();
+
+    formData.append("Ubicacion_idUbicacion", this.idLocalizacion);
+    formData.append("mejorasEquipo", this.mejoras.value);
+    formData.append("codigo", this.codigo.value);
+    formData.append("fechaAdquisicion", this.fechaAdquisicion.value);
+    formData.append("observaciones", this.observaciones.value)
+    formData.append("etiqueta", this.etiqueta.value);
+
+    this.objectS.addObject(formData, this.idGrupoObjetos).subscribe(
+      (res: any) => {
+        return;
+      });
+  }
 }
--- inventarium/src/app/services/objects.service.ts
+++ inventarium/src/app/services/objects.service.ts
@@ -8,36 +8,36 @@ import { Objeto } from '../interfaces/objeto';
 })
 export class ObjectsService {
   _url = "api/objeto"
-  
-  constructor(private http : HttpClient, private userService: UserService) { }
 
-  getObjects(idGrupoObjetos: number){
-    let header = new HttpHeaders(({'Content-Type': 'application/json',"user_token":this.userService.getToken()}));
-    return this.http.get(this._url+"/"+idGrupoObjetos, { headers : header});
+  constructor(private http: HttpClient, private userService: UserService) { }
+
+  getObjects(idGrupoObjetos: number) {
+    let header = new HttpHeaders(({ 'Content-Type': 'application/json', "user_token": this.userService.getToken() }));
+    return this.http.get(this._url + "/" + idGrupoObjetos, { headers: header });
   }
 
-  getObject(idObjeto: number){
-    let header = new HttpHeaders(({'Content-Type': 'application/json',"user_token":this.userService.getToken()}));
-    return this.http.get(this._url+"/id/"+idObjeto,{ headers : header});
+  getObject(idObjeto: number) {
+    let header = new HttpHeaders(({ 'Content-Type': 'application/json', "user_token": this.userService.getToken() }));
+    return this.http.get(this._url + "/id/" + idObjeto, { headers: header });
   }
 
-  getObjectsByLocation(idUbicacion: number){
-    let header = new HttpHeaders(({'Content-Type': 'application/json',"user_token":this.userService.getToken()}));
-    return this.http.get(this._url+"/location-id/"+idUbicacion,{ headers : header});
+  getObjectsByLocation(idUbicacion: number) {
+    let header = new HttpHeaders(({ 'Content-Type': 'application/json', "user_token": this.userService.getToken() }));
+    return this.http.get(this._url + "/location-id/" + idUbicacion, { headers: header });
   }
 
-  addObject(object: Objeto){
-    let header = new HttpHeaders(({'Content-Type': 'application/json',"user_token":this.userService.getToken()}));
-    return this.http.post(this._url, object, { headers : header});
+  addObject(objectGroup: FormData, idGrupoObjetos: number) {
+    let header = new HttpHeaders(({ 'Content-Type': 'application/json', "user_token": this.userService.getToken() }));
+    return this.http.post(this._url + "/" + idGrupoObjetos, objectGroup);
   }
 
-  deleteObject(idObjetos: number){
-    let header = new HttpHeaders(({'Content-Type': 'application/json',"user_token":this.userService.getToken()}));
-    return this.http.delete(this._url+"/"+idObjetos,{ headers : header});
+  deleteObject(idObjetos: number) {
+    let header = new HttpHeaders(({ 'Content-Type': 'application/json', "user_token": this.userService.getToken() }));
+    return this.http.delete(this._url + "/" + idObjetos, { headers: header });
   }
 
-  updateObject(object: Objeto){
-    let header = new HttpHeaders(({'Content-Type': 'application/json',"user_token":this.userService.getToken()}));
-    return this.http.put(this._url+"/"+object.idObjeto,object,{ headers : header});
+  updateObject(object: Objeto) {
+    let header = new HttpHeaders(({ 'Content-Type': 'application/json', "user_token": this.userService.getToken() }));
+    return this.http.put(this._url + "/" + object.idObjeto, object, { headers: header });
   }
 }
