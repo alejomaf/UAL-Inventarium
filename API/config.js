b9d22b3c8b7f2f070f3a753029ba6752c34eb87b
Alejo Martín Arias Filippo <alejomaf@outlook.com>
2022-01-06 16:20:46 +0100

    Finishing inventarium setup

--- API/config.js
+++ API/config.js
@@ -2,10 +2,10 @@ const env = process.env;
 
 const config = {
   db: {
-    host: env.DB_HOST || 'localhost',
-    user: env.DB_USER || 'ualinventarium',
-    password: env.DB_PASSWORD || 'uS3rP4ssM0Ord20D1f1CIL3',
-    database: env.DB_NAME || 'ualinventarium'
+    host: 'localhost',
+    user: 'ualinventarium',
+    password: 'uS3rP4ssM0Ord20D1f1CIL3',
+    database: 'ualinventarium'
   },
   listPerPage: env.LIST_PER_PAGE || 10,
 };
