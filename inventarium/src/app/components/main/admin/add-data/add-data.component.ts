e5aa97b6ba83ae9838314474c70ba76689a7a2ad
alejomaf <alejomaf@outlook.com>
2022-02-05 13:41:16 +0100

    Procesamiento de ficheros Excel habilitado

--- inventarium/src/app/components/main/admin/add-data/add-data.component.ts
+++ inventarium/src/app/components/main/admin/add-data/add-data.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConfiguracionProcesada, GrupoObjetosProcesados, KitProcesado, ObjetosProcesados } from 'src/app/interfaces/grupoobjetosprocesados';
 import * as XLSX from 'xlsx';
 
 @Component({
@@ -10,6 +11,12 @@ export class AddDataComponent implements OnInit {
 
   excel: any;
   fileName: any;
+
+  processed_data: any;
+  processed_location: { nombre: string, id: number }[] = []
+  processed_objects: GrupoObjetosProcesados[] = [];
+  object_visualizer: any;
+  ultimoObjeto = "";
 
   constructor() { }
 
@@ -32,12 +39,126 @@ export class AddDataComponent implements OnInit {
     fetch(this.excel).then(function (res) {
       if (!res.ok) throw new Error("Fetch of the file failed");
       return res.arrayBuffer();
-    }).then(function (ab) {
+    }).then((ab) => {
       var data = new Uint8Array(ab);
       var datos = XLSX.read(data, { type: "array" });
-      var datos_procesados = datos.SheetNames;
-      console.log(XLSX.utils.sheet_to_json(datos.Sheets[datos_procesados[0]]));
-    });
+      this.processed_data = XLSX.utils.sheet_to_json(datos.Sheets[datos.SheetNames[0]]);
+      console.log(this.processed_data);
+      this.processLocations();
 
+    });
+  }
+
+  processObjects() {
+    var isKit = false;
+    for (let i = 0; i < this.processed_data.length; i++) {
+      let obj = this.processed_data[i];
+
+      //Kit part
+      if (isKit) {
+        if (obj.nombre == "fin-kit") {
+          //Stoping the iteration
+          isKit = false;
+          continue;
+        } else {
+          //Iterating througt kits elements
+          this.addKit(obj);
+        }
+        continue;
+      }
+
+      if (obj.nombre == "kit") {
+        //Starting the iteration
+        isKit = true;
+        //Getting the name, location and quantity
+        i++;
+        obj = this.processed_data[i];
+        this.addGroupOfObject(obj);
+        for (let i = 0; i < obj.cantidad; i++) {
+          this.addObject(obj);
+        }
+        continue;
+      }
+
+      if (this.processed_objects.length == 0) {
+        this.addGroupOfObject(obj);
+        continue;
+      }
+
+      if (obj.nombre === this.ultimoObjeto)
+        if (this.processed_objects[this.processed_objects.length - 1].nombre === this.ultimoObjeto) {
+          //We add the object
+          this.addObject(obj);
+        } else {
+          //We create the group of objects and add the object
+          this.addGroupOfObject(obj);
+        }
+      else this.addGroupOfObject(obj);
+
+    }
+    console.log(this.processed_objects);
+  }
+
+  addGroupOfObject(grp_obj: any) {
+    this.ultimoObjeto = grp_obj.nombre;
+    var grp_obj_aux: GrupoObjetosProcesados = { imagen: grp_obj.imagen, marca: grp_obj.marca, modelo: grp_obj.modelo, nombre: grp_obj.nombre, objetos: [], tipo: grp_obj.tipo == undefined ? 0 : grp_obj.tipo, kit: grp_obj.cantidad > 0 ? [] : undefined };
+    this.processed_objects.push(grp_obj_aux);
+    this.addObject(grp_obj);
+  }
+
+  addObject(obj: any) {
+    var obj_aux: ObjetosProcesados = { codigo: obj.codigo, configuracion: obj.configuracion, etiqueta: obj.etiqueta, fechaAdquisicion: obj.fechaAdquisicion, mejorasEquipo: obj.mejorasEquipo, observaciones: obj.observaciones, organizativa: obj.organizativa, Ubicacion_idUbicacion: obj.ubicacion == undefined ? 1 : this.getLocation(obj.ubicacion) };
+
+    if (this.itContainsConfig(obj)) {
+      var conf_aux: ConfiguracionProcesada = { armario: obj.armario, boca: obj.boca, contrasena: obj.contrasena, ip: obj.ip, mac: obj.mac, usuario: obj.usuario };
+      obj_aux.configuracion = conf_aux;
+    }
+    this.processed_objects[this.processed_objects.length - 1].objetos.push(obj_aux);
+  }
+
+  addKit(kit: any) {
+    var kit_aux: KitProcesado = { cantidad: kit.cantidad, imagen: kit.imagen, nombre: kit.nombre, observaciones: kit.observaciones };
+    this.processed_objects[this.processed_objects.length - 1].kit?.push(kit_aux);
+  }
+
+
+  processLocations() {
+    for (let loc of this.processed_data) {
+      if (loc.ubicacion == undefined) continue;
+      if (this.locationIsAdded(loc.ubicacion)) continue;
+      this.processed_location.push({ nombre: loc.ubicacion, id: 0 });
+    }
+    console.log(this.processed_location);
+  }
+
+  locationIsAdded(location: String) {
+    for (let loc_exist of this.processed_location) {
+      if (location === loc_exist.nombre) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  getSentLocation(processed_location: { nombre: string, id: number }, location_selected: number) {
+    console.log(location_selected);
+    processed_location.id = location_selected;
+  }
+
+  getLocation(nombre: String): number {
+    for (let loc of this.processed_location) {
+      if (loc.nombre === nombre) return loc.id;
+    }
+    return 1;
+  }
+
+  itContainsObject(object: any): boolean {
+    if (object.mejorasEquipo == undefined && object.codigo == undefined && object.fechaAdquisicion == undefined && object.observaciones == undefined && object.etiqueta == undefined) return false;
+    return true;
+  }
+
+  itContainsConfig(configuration: any): boolean {
+    if (configuration.ip == undefined && configuration.mac == undefined && configuration.boca == undefined && configuration.armario == undefined && configuration.usuario == undefined && configuration.contrasena == undefined) return false;
+    return true;
   }
 }
---
+++ inventarium/src/app/interfaces/grupoobjetosprocesados.ts
@@ -0,0 +1,36 @@
+export interface GrupoObjetosProcesados {
+    nombre: string,
+    imagen: string,
+    marca: string,
+    modelo: string,
+    tipo: number,
+    objetos: ObjetosProcesados[],
+    kit?: KitProcesado[],
+}
+
+export interface ObjetosProcesados {
+    mejorasEquipo: string,
+    codigo: string,
+    fechaAdquisicion: Date,
+    observaciones: string,
+    organizativa: string,
+    etiqueta: string,
+    Ubicacion_idUbicacion: number,
+    configuracion: ConfiguracionProcesada,
+}
+
+export interface ConfiguracionProcesada {
+    ip: string,
+    mac: string,
+    boca: string,
+    armario: string,
+    usuario: string,
+    contrasena: string,
+}
+
+export interface KitProcesado {
+    nombre: string,
+    cantidad: number,
+    imagen: string,
+    observaciones: string
+}
